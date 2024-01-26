ecef49180020125521cb12ad94d7f838356c7cd2
Aman Singh <amans9863@gmail.com>
2024-01-26 23:27:20 +0530

    commit 26012024

--- App.js
+++ App.js
@@ -1,21 +1,74 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, Pressable, TouchableHighlight, Dimensions } from 'react-native';
+import { Quicksand_300Light } from "@expo-google-fonts/quicksand";
+import { useFonts } from "expo-font";
 
 export default function App() {
   //npx expo start --tunnel
+  let [fontsLoaded, fontError] = useFonts({
+    Quicksand_300Light,
+    'single-day': require('./assets/fonts/SingleDay-Regular.ttf'),
+    'rubik-glitch-pop': require('./assets/fonts/RubikGlitchPop-Regular.ttf')
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return <Text>Loading...</Text>;
+  }
+
+  alertMessage = ()=>{Alert.alert('I Love You Pooja!')}
+
   return (
-    <View style={styles.container}>
-      <Text>Working Aman!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container1}>
+        <Text style={styles.headerText}>meetup</Text>
+        <Text style={styles.versionText}>v1.0.0</Text>
+      </View>
+      <View style={styles.container2}>
+          <TouchableHighlight
+              style = {{
+                borderRadius: 25,
+                width: 50,
+                height: 50,
+                backgroundColor:'#f5f5f5',
+                justifyContent: 'center',
+                alignItems: 'center'
+              }}
+              underlayColor = '#ccc'
+              onPress = { alertMessage }
+            >
+            <Text>Start</Text>
+        </TouchableHighlight>
+      </View>
+      
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container1: {
+    backgroundColor: 'white',
+    height: 70,
+    paddingLeft: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#c4c1c0',
+    borderRadius: 10
   },
+  headerText: {
+    color: 'black',
+    fontSize: 20,
+    top: 25,
+    fontFamily: 'rubik-glitch-pop',
+    fontSize: 35,
+  },
+  versionText: { 
+    textAlign:'right',
+    right: 10,
+    fontFamily: 'Quicksand_300Light'
+  },
+  container2:{
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderCurve:10
+  }
 });
